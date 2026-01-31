474b37c7eba2bd3cf7bc1281b45e044d4561efe7
Quonain <rk586705@gmail.com>
2026-01-31 15:47:23 +0530

    Time: 37 ms (22.23%) | Memory: 156 MB (17.50%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -7,15 +7,15 @@ class Solution {
       int n = nums.length;
       int [] result=new int[n-k+1];
       Deque<Integer>deque=new LinkedList<>();
-      for(int i=0;i<n;i++){
+      for(int i=0;i<n;i++){//sb kch example le ke smjh k=3,r i=2 ho gya maanle sbse bda element 0index m h
        while(!deque.isEmpty() && deque.peek() < i - k + 1){
         deque.poll();
        }
-
+ //agr av wala element sbse bda h toh sbko nikalo but check from top to bottom
        while(!deque.isEmpty() && nums[i] > nums[deque.peekLast()]){
         deque.pollLast();
        }
-       deque.offer(i);
+       deque.offer(i);//last me add kr rha mtlb upr 
        if(i>=k-1){
         result[i-k+1]=nums[deque.peek()];
        }
