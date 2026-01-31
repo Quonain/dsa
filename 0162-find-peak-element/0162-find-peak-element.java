19fb191a54233702d0618c2daa7338955c84e2ac
Quonain <rk586705@gmail.com>
2026-02-01 01:31:46 +0530

    Time: 0 ms (100%), Space: 44.6 MB (12.74%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+       int low=1;
+       int n=nums.length-1;
+       int high=nums.length-2;
+       if(nums.length==1){
+        return 0;
+       }
+       
+       if(nums[0]>nums[1]){
+        return 0;
+       }
+       if(nums[n]>nums[n-1]){
+        return n;
+       }
+       while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+            return mid;
+        }
+        if(nums[mid]>nums[mid+1]){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+       }
+       return -1; 
+    }
+}
