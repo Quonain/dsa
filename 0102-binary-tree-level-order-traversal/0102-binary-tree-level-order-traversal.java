3d92c55ad16a4dd11888d62a6637b171385311b6
Quonain <rk586705@gmail.com>
2026-02-10 20:48:53 +0530

    Time: 1 ms (95.59%), Space: 46.8 MB (59.37%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,27 +15,28 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root==null){
-            return ans;
-        }
-        Queue<TreeNode> q = new LinkedList <>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int size=q.size();
-            List<Integer> level = new ArrayList<>();
-            for(int i = 0; i<size ; i++){
-                TreeNode node = q.poll();
-                level.add(node.val);
-                if(node.left != null){
-                    q.add(node.left);
-                }
-                if(node.right !=null){
-                    q.add(node.right);
-                }
-            }
-            ans.add(level);
-        }
+     List<List<Integer>> ans = new ArrayList<>();
+     if(root == null){
         return ans;
-    }
+     }
+     Queue<TreeNode> q = new LinkedList<>();
+     q.add(root);
+     while(!q.isEmpty()){
+        //printing watever is in the queue in one level && same time we are checking and adding if their exist their right or left and if yes we are adding it to q for for printing next level
+        int size = q.size();//q size determines the no. of element in that level
+        List <Integer> level = new ArrayList<>();
+        for(int i=0; i<size; i++){
+            TreeNode node = q.poll();
+            level.add(node.val);
+            if(node.left != null){
+                q.add(node.left);
+            }
+            if(node.right != null){
+                q.add(node.right);
+            }
+        }
+        ans.add(level);
+     }
+     return ans;
+}
 }
