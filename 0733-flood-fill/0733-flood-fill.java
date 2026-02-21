473d8bac8301a4194d1c4697d543cc04bb916f8f
Quonain <rk586705@gmail.com>
2026-02-21 17:50:52 +0530

    Time: 0 ms (100%), Space: 46.9 MB (63.92%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,26 +1,26 @@
 class Solution {
-    private void dfs(int row,int col,int[][] image,int[][] ans ,int color, int initialColor,
-    int [] delRow, int [] delCol){
-        ans[row][col]= color;
-        int n = image.length;
-        int m = image[0].length;
-        for(int i = 0;i< 4 ; i++){
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
-            if(nrow < n && nrow>=0 && ncol < m && ncol>=0 && 
-            image[nrow][ncol]==initialColor && 
-            ans[nrow][ncol]!=color){
-                dfs(nrow,ncol,image,ans,color,initialColor,delRow,delCol);
+    private void dfs(int[][] images, int r, int c,int color,int initial){
+        images[r][c]=color;
+        int n = images.length;
+        int m = images[0].length;
+        int [] delrow = {-1,0,1,0};
+        int [] delcol = {0,1,0,-1};
+        for(int i =0 ; i< 4; i++){
+            int nrow = r+delrow[i];
+            int ncol = c+delcol[i];
+            if(nrow<n && nrow >= 0 && ncol < m && ncol >=0 && images[nrow][ncol]==initial){
+                dfs(images,nrow,ncol,color,initial);
             }
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int initialColor = image[sr][sc];
-        int [][] ans = image;
-        int [] delRow = {-1,0,1,0};
-        int [] delCol ={0,1,0,-1};
-        dfs(sr,sc,image,ans,color,initialColor,delRow,delCol);
-        return ans;
+         
+        int initial = image[sr][sc];
+        if(initial == color){
+            return image;
+        }
+        dfs(image, sr,sc,color,initial);
+        return image;
 
     }
 }
